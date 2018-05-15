2db54edebd31fa933ba55dcbd52f6abdfb6a237d
wangqinqin <wangqinqin@idcos.com>
2018-05-15 16:15:43 +0800

    create testwqq

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -3,7 +3,6 @@ import { FormattedMessage } from 'react-intl';
 
 import NavBar from './NavBar';
 import HeaderLink from './HeaderLink';
-import Banner from './banner.jpg';
 import messages from './messages';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
